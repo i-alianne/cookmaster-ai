47c4ca051407fcc21a2a1e00b190297cce6d515a
Alianne Dosal Allison <adosalallison@gmail.com>
2024-06-22 21:11:17 +0200

    Added basic form handling and Typewriter

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
+function generateRecipe(event) {
+  event.preventDefault();
 
+  new Typewriter("#recipe", {
+    strings: "Tomato Basil Pasta",
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+  });
+}
+
+let recipeForm = document.querySelector("#recipe-generator-form");
+recipeForm.addEventListener("submit", generateRecipe);
