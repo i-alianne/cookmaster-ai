7c660b1a1b3f6cb745cfa6ea8c26c3e2169ba1df
Alianne Dosal Allison <adosalallison@gmail.com>
2024-06-22 21:55:02 +0200

    Added the AI API Integration

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
-function generateRecipe(event) {
-  event.preventDefault();
-
+function displayRecipe(response) {
   new Typewriter("#recipe", {
-    strings: "Tomato Basil Pasta",
+    strings: response.data.answer,
     autoStart: true,
     delay: 1,
     cursor: "",
   });
+}
+
+function generateRecipe(event) {
+  event.preventDefault();
+
+  let apiKey = "be427b594372a38b6f2t7o035dc1e24d";
+  let context =
+    "You are an expert chef who loves creating short, easy-to-make recipes. Your task is to generate a concise recipe that incorporates the ingredient provided by the user. Ensure that the recipe title does not include the words 'recipe' or 'easy'. Present the recipe in basic HTML format. Do not use ";
+  let prompt = `User ingredient: ${instructions.value}. Generate a short, easy-to-follow recipe using this ingredient.`;
+  let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  axios.get(apiURL).then(displayRecipe);
 }
 
 let recipeForm = document.querySelector("#recipe-generator-form");
