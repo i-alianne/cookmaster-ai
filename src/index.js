75122c201f39dc935efb4dd29631985e7be2188a
Alianne Dosal Allison <adosalallison@gmail.com>
2024-10-07 12:50:09 +0200

    Fixed bugs

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 function displayRecipe(response) {
+  let recipeContent = response.data.answer;
+
+  recipeContent = recipeContent.replace(/```html/g, "").trim();
+
   new Typewriter("#recipe", {
-    strings: response.data.answer,
+    strings: recipeContent,
     autoStart: true,
     delay: 1,
     cursor: "",
@@ -12,7 +16,7 @@ function generateRecipe(event) {
 
   let apiKey = "be427b594372a38b6f2t7o035dc1e24d";
   let context =
-    "You are an expert chef who loves creating short, easy-to-make recipes. Your task is to generate a concise recipe that incorporates the ingredient provided by the user. Ensure that the recipe title does not include the words 'recipe' or 'easy'. Present the recipe in basic HTML format. Do not use ";
+    "You are an expert chef who loves creating short, easy-to-make recipes. Your task is to generate a concise recipe that incorporates the ingredient provided by the user. Ensure that the recipe title does not include the words 'recipe' or 'easy'. Present the recipe in basic HTML format.";
   let prompt = `User ingredient: ${instructions.value}. Generate a short, easy-to-follow recipe using this ingredient.`;
   let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
